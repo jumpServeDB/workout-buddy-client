b90ae7f5b1c47cfdfdbc1c877747f31ba67dafa0
jumpServeDB <dbalint88@proton.me>
2026-02-17 21:36:39 -0100

    point to railway server

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://workout-buddy-server-production.up.railway.app:4000",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
