1138a5a783423cab638d198f921a101387763f54
jumpServeDB <dbalint88@proton.me>
2026-02-17 22:05:07 -0100

    trying to deploy to vercel

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/workout-buddy-client">
         <Navbar />
         <div className="pages">
           <Routes>
